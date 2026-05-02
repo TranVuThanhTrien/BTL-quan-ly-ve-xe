5bdaa51daebdfb438e57f7c6ea5311b3d8c7064b
TranVuThanhTrien <tranvuthanhtrien@gmail.com>
2026-05-02 21:58:42 +0700

    file header klhoi tao thu vien va macro

---
+++ vexe.h
@@ -0,0 +1,20 @@
+#ifndef VEXE_H
+#define VEXE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
+#ifndef VEXE_H
+#define VEXE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Macro va tien xu ly 
+#define MAX 100
+#define FILE_BIN "data_vexe.dat"
+
+#endif
