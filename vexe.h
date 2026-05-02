137c23e1c0dd9a5ed0a94a6754ba0e20a5d3241b
TranVuThanhTrien <tranvuthanhtrien@gmail.com>
2026-05-03 00:15:10 +0700

    bo sung struct

--- vexe.h
+++ vexe.h
@@ -18,3 +18,24 @@
 #define FILE_BIN "data_vexe.dat"
 
 #endif
+#ifndef VEXE_H
+#define VEXE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Macro va tien xu ly 
+#define MAX 100
+#define FILE_BIN "data_vexe.dat"
+
+// Cau truc danh sach lien ket don 
+typedef struct VeXe {
+    char maVe[10];
+    char tenKhach[MAX];
+    char tuyenDuong[MAX];
+    float giaVe;
+    struct VeXe *next; // Cau truc tu tro
+} VeXe;
+
+#endif
