dfe2d85372f6ddd35e2606fcb5f4988c2d5546aa
TranVuThanhTrien <tranvuthanhtrien@gmail.com>
2026-05-03 23:06:16 +0700

    Khai bao cac ham va sd ham de quy

--- vexe.h
+++ vexe.h
@@ -39,3 +39,34 @@ typedef struct VeXe {
 } VeXe;
 
 #endif
+#ifndef VEXE_H
+#define VEXE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Macro va tien xu ly 
+#define MAX 100
+#define FILE_BIN "data_vexe.dat"
+
+// Cau truc danh sach lien ket don 
+typedef struct VeXe {
+    char maVe[10];
+    char tenKhach[MAX];
+    char tuyenDuong[MAX];
+    float giaVe;
+    struct VeXe *next; // Cau truc tu tro
+} VeXe;
+
+// Khai bao ham 
+VeXe* taoNode();
+void themVe(VeXe **head);
+void hienThi(VeXe *head);
+void xoaVe(VeXe **head, char *ma);
+void luuFile(VeXe *head);
+void docFile(VeXe **head);
+void giaiPhong(VeXe *head);
+int demVeDeQuy(VeXe *head); // Ham de quy
+
+#endif
