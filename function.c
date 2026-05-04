a7ad7cb0ba770e62c4d04b56c527a11ebb1ad02e
NguyenBao Tri <kiaranz060807@gmail.com>
2026-05-04 16:16:27 +0700

    Create Function.c
    -Them ham taonode de cap phat bo nho dong
    -Them ham clearbuffer de xy ly troi lenh
    -Them hm taoVe ( nhap thong tin khach hang, tuyen duong)
    -Them ham xoaVe de giai phong bo nho)
    -Them ham hienThi de kiem tra du lieu

---
+++ function.c
@@ -0,0 +1,55 @@
+#include "vexe.h"
+// Ham xoa bo nho dem, tranh troi lenh
+void xoaDem() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+// Cap phát bo nho dong
+VeXe* taoNode() {
+    VeXe *newNode = (VeXe*)malloc(sizeof(VeXe));
+    if (newNode == NULL) return NULL;
+    
+    printf("Nhap ma ve: "); scanf("%s", newNode->maVe);
+    xoadem(); // Xoa bo nho dem
+    printf("Nhap ten khach hang: "); fgets(newNode->tenKhach, MAX, stdin);
+    newNode->tenKhach[strcspn(newNode->tenKhach, "\n")] = 0;
+    printf("Nhap tuyen duong: "); fgets(newNode->tuyenDuong, MAX, stdin);
+    newNode->tuyenDuong[strcspn(newNode->tuyenDuong, "\n")] = 0;
+    printf("Nhap gia ve: "); scanf("%f", &newNode->giaVe);
+    
+    newNode->next = NULL;
+    return newNode;
+}
+// them ve vao cuoi danh sach
+void themVe(VeXe **head) {
+    VeXe *newV = taoNode();
+    if (*head == NULL) {
+        *head = newV;
+    } else {
+        VeXe *temp = *head;
+        while (temp->next != NULL)
+        temp = temp->next;
+    }
+        temp->next = newV;
+}
+//Xoa ve theo ma ve
+void xoaVe(VeXe **head, char *ma) {
+    VeXe *temp = *head, *prev = NULL;
+    while (temp != NULL && strcmp(temp->maVe, ma) != 0) {
+        prev = temp;
+        temp = temp->next;
+    }
+    if (temp == NULL) return;
+    if (prev == NULL) *head = temp->next;
+    else prev->next = temp->next;
+    free(temp); // Giai phong bo nho 
+}
+//Hien thi danh sach ve
+void hienThi(VeXe *head) {
+    Vexe *temp = head // dung bien tam
+    printf("\n%-10s %-20s %-20s %-10s\n", "Ma Ve", "Ten Khach", "Tuyen Duong", "Gia");
+    while (temp != NULL) {
+        printf("%-10s %-20s %-20s %-10.2f\n", temp->maVe, temp->tenKhach, temp->tuyenDuong, head->giaVe);
+        temp = temp->next;
+    }
+}
