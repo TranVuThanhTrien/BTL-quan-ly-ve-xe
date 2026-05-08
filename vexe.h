28a6edf8d74cceed08ac294ea88f5b22a45fa5cc
TranVuThanhTrien <tranvuthanhtrien@gmail.com>
2026-05-08 17:53:33 +0700

    fix loi lap

--- vexe.h
+++ vexe.h
@@ -4,75 +4,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
-#endif
-#ifndef VEXE_H
-#define VEXE_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Macro va tien xu ly 
 #define MAX 100
 #define FILE_BIN "data_vexe.dat"
 
-#endif
-#ifndef VEXE_H
-#define VEXE_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Macro va tien xu ly 
-#define MAX 100
-#define FILE_BIN "data_vexe.dat"
-
-// Cau truc danh sach lien ket don 
+// Cau truc danh sach lien ket don
 typedef struct VeXe {
     char maVe[10];
     char tenKhach[MAX];
     char tuyenDuong[MAX];
     float giaVe;
-    struct VeXe *next; // Cau truc tu tro
+    struct VeXe *next; 
 } VeXe;
 
-#endif
-#ifndef VEXE_H
-#define VEXE_H
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Macro va tien xu ly 
-#define MAX 100
-#define FILE_BIN "data_vexe.dat"
-
-// Cau truc danh sach lien ket don 
-typedef struct VeXe {
-    char maVe[10];
-    char tenKhach[MAX];
-    char tuyenDuong[MAX];
-    float giaVe;
-    struct VeXe *next; // Cau truc tu tro
-} VeXe;
-
-// Khai bao ham 
+// Khai bao cac ham trong function.c
+void xoaDem();
 VeXe* taoNode();
 void themVe(VeXe **head);
 void hienThi(VeXe *head);
 void xoaVe(VeXe **head, char *ma);
+
+// Khai bao cac ham trong storage.c
 void luuFile(VeXe *head);
 void docFile(VeXe **head);
+
+// Khai bao cac ham trong utils.c
 void giaiPhong(VeXe *head);
-int demVeDeQuy(VeXe *head); // Ham de quy
-// utils.c prototypes
+int demVeDeQuy(VeXe *head);
 int validatePrice(const char *input);
-int validateDate(const char *date);
-int countTicketsRecursive(Ticket *head);
-Ticket* searchByNameRecursive(Ticket *head, const char *name);
-void freeAllTickets(Ticket *head);
 
 #endif
